ce9a848105471281f815b7f21c43c86f47481c95
kdotamoh <i.be.your.strychnine@gmail.com>
2020-04-16 17:17:16 +0000

    add intercom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import Intercom from "react-intercom";
+
 import GlobalStyles from "./GlobalStyles";
 import ScrollToTop from "components/ScrollToTop";
-
 import theme from "app-constants/theme";
 
 import store from "store";
@@ -156,7 +157,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-
+      <Intercom appID="kr96mkb4" />
       <Router>
         <ScrollToTop />
         <Switch>

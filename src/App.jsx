a452f332c06b4d63b6817e7bbf0e77f5f06605f8
Emeka Ndukwe <chukwuemekandukwe01@gmail.com>
2026-09-15 08:23:12 +0000

    Modernize build and remediate dependency alerts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import Intercom from "react-intercom";
 
 import GlobalStyles from "./GlobalStyles";
 import ScrollToTop from "components/ScrollToTop";
@@ -177,7 +176,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Intercom appID="kr96mkb4" />
       <Router>
         <ScrollToTop />
         <Switch>
